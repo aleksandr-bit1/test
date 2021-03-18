6b9ffa8e44f6355b7e27b5492a6249c6ab2c1d3b
aleksandr.bit <aleksandr.bit@gmail.com>
2021-03-18 20:39:50 +0300

    script and report

---
+++ Reports/searchindex.js
@@ -0,0 +1,290 @@
+
+function CreateFileRefs()
+{
+var fr=new Array();
+            
+fr[1]=['http://integration.aeroclub.ru/hub/schemas/profiles', 'http___integration_aeroclub_ru_hub_schemas_profiles.html'];
+fr[2]=['http://integration.aeroclub.ru/hub/schemas/services', 'http___integration_aeroclub_ru_hub_schemas_services.html'];
+fr[3]=['document.xsd', 'document_xsd.html'];
+fr[4]=['document', 'document.html'];
+fr[5]=['citizenship', 'citizenship.html'];
+fr[6]=['code', 'code.html'];
+fr[7]=['iso3611-a2', 'iso3611-a2.html'];
+fr[8]=['expiresOn', 'expireson.html'];
+fr[9]=['firstName', 'firstname.html'];
+fr[10]=['issuedOn', 'issuedon.html'];
+fr[11]=['lastName', 'lastname.html'];
+fr[12]=['number', 'number.html'];
+fr[13]=['placeOfBirth', 'placeofbirth.html'];
+fr[14]=['series', 'series.html'];
+fr[15]=['type', 'type.html'];
+fr[16]=['service_new.xsd', 'service_new_xsd.html'];
+fr[17]=['aeroexpressStationRef', 'aeroexpressstationref.html'];
+fr[18]=['type', 'type1.html'];
+fr[19]=['id', 'id.html'];
+fr[20]=['airport', 'airport.html'];
+fr[21]=['city', 'city.html'];
+fr[22]=['id', 'id1.html'];
+fr[23]=['airportRef', 'airportref.html'];
+fr[24]=['airport', 'airport1.html'];
+fr[25]=['city', 'city1.html'];
+fr[26]=['id', 'id2.html'];
+fr[27]=['country', 'country.html'];
+fr[28]=['code', 'code1.html'];
+fr[29]=['name', 'name.html'];
+fr[30]=['name', 'name1.html'];
+fr[31]=['english', 'english.html'];
+fr[32]=['russian', 'russian.html'];
+fr[33]=['property', 'property.html'];
+fr[34]=['identifier', 'identifier.html'];
+fr[35]=['value', 'value.html'];
+fr[36]=['railwayStation', 'railwaystation.html'];
+fr[37]=['country', 'country1.html'];
+fr[38]=['name', 'name2.html'];
+fr[39]=['railwayStationRef', 'railwaystationref.html'];
+fr[40]=['id', 'id3.html'];
+fr[41]=['report', 'report.html'];
+fr[42]=['service', 'service.html'];
+fr[43]=['service', 'service1.html'];
+fr[44]=['isOffline', 'isoffline.html'];
+fr[45]=['type', 'type2.html'];
+fr[46]=['aeroexpress', 'aeroexpress.html'];
+fr[47]=['arrivalTo', 'arrivalto.html'];
+fr[48]=['departureFrom', 'departurefrom.html'];
+fr[49]=['avia', 'avia.html'];
+fr[50]=['legs', 'legs.html'];
+fr[51]=['leg', 'leg.html'];
+fr[52]=['arrivalTo', 'arrivalto1.html'];
+fr[53]=['departureFrom', 'departurefrom1.html'];
+fr[54]=['hotel', 'hotel.html'];
+fr[55]=['cityId', 'cityid.html'];
+fr[56]=['rail', 'rail.html'];
+fr[57]=['arrivalTo', 'arrivalto2.html'];
+fr[58]=['departureFrom', 'departurefrom2.html'];
+fr[59]=['analytics', 'analytics.html'];
+fr[60]=['code', 'code2.html'];
+fr[61]=['isTravelPolicyCompliant', 'istravelpolicycompliant.html'];
+fr[62]=['authorization', 'authorization.html'];
+fr[63]=['completedAt', 'completedat.html'];
+fr[64]=['stages', 'stages.html'];
+fr[65]=['stage', 'stage.html'];
+fr[66]=['authorizers', 'authorizers.html'];
+fr[67]=['authorizer', 'authorizer.html'];
+fr[68]=['accountEmailAddress', 'accountemailaddress.html'];
+fr[69]=['completedAt', 'completedat1.html'];
+fr[70]=['businessTripNumber', 'businesstripnumber.html'];
+fr[71]=['document', 'document1.html'];
+fr[72]=['dateIssue', 'dateissue.html'];
+fr[73]=['exchangedNumber', 'exchangednumber.html'];
+fr[74]=['number', 'number1.html'];
+fr[75]=['endsAt', 'endsat.html'];
+fr[76]=['files', 'files.html'];
+fr[77]=['file', 'file.html'];
+fr[78]=['link', 'link.html'];
+fr[79]=['name', 'name3.html'];
+fr[80]=['contentType', 'contenttype.html'];
+fr[81]=['language', 'language.html'];
+fr[82]=['journeyNumber', 'journeynumber.html'];
+fr[83]=['operation', 'operation.html'];
+fr[84]=['status', 'status.html'];
+fr[85]=['orderNumber', 'ordernumber.html'];
+fr[86]=['serviceNumber', 'servicenumber.html'];
+fr[87]=['startsAt', 'startsat.html'];
+fr[88]=['stcd1', 'stcd1.html'];
+fr[89]=['stcd3', 'stcd3.html'];
+fr[90]=['tariff', 'tariff.html'];
+fr[91]=['currency', 'currency.html'];
+fr[92]=['total', 'total.html'];
+fr[93]=['totalNoVat', 'totalnovat.html'];
+fr[94]=['transferRef', 'transferref.html'];
+fr[95]=['airport', 'airport2.html'];
+fr[96]=['location', 'location.html'];
+fr[97]=['railwayStation', 'railwaystation1.html'];
+fr[98]=['report', 'report1.html'];    
+ return fr;          
+}
+
+function CreateWordIndex()
+{
+var w=[
+            
+['schema',[3,16]],
+['xmlns',[3,16]],
+['http',[3,4,5,6,7,16]],
+['xmlschema',[3,16]],
+['integration',[3,16]],
+['aeroclub',[3,16]],
+['schemas',[3,16]],
+['profiles',[3,16]],
+['targetnamespace',[3,16]],
+['elementformdefault',[3,16]],
+['qualified',[3,16]],
+['complextype',[3,4,5,6,16,17,20,23,25,27,30,33,36,39,41,43,46,49,50,51,54,56,59,62,64,65,66,67,71,76,77,83,90,94]],
+['name',[3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98]],
+['document',[3,4,16,42,43,71]],
+['element',[3,4,5,6,7,8,9,10,11,12,13,14,16,17,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98]],
+['series',[3,4,14]],
+['type',[3,4,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,82,85,86,87,88,89,90,91,92,93,94,95,96,97,98]],
+['string',[3,4,5,6,7,9,11,12,13,14,15,16,17,18,27,28,30,31,32,33,34,35,43,45,62,64,65,66,67,68,71,73,74,76,77,79,80,81,83,84,90,91,94,96]],
+['nillable',[3,4,8,9,10,11,13,14,16,43,59,61,62,70,71,75,76,82,87]],
+['true',[3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98]],
+['number',[3,4,12,16,43,71,74]],
+['issuedon',[3,4,10]],
+['date',[3,4,8,10]],
+['expireson',[3,4,8]],
+['citizenship',[3,4,5]],
+['minoccurs',[3,4,5,6,7,16,36,37,43,49,50,51,62,64,65,66,67,76,77,94,95,97]],
+['maxoccurs',[3,4,5,6,7,16,36,37,43,49,50,51,62,64,65,66,67,76,77,94,95,97]],
+['sequence',[3,4,5,16,17,23,39,41,43,46,49,50,51,54,56,59,62,64,65,66,67,71,76,90]],
+['code',[3,4,5,6,7,16,27,28,37,43,59,60]],
+['iso3611-a2',[3,4,5,6,7]],
+['annotation',[3,4,5,6,7,16,33,34,35,43,44,45,59,60,61,62,63,64,65,69,70,71,72,73,74,76,77,78,79,82,83,84,85,86,88,89,90,91,92,93]],
+['documentation',[3,4,5,6,7,16,33,34,35,43,44,45,59,60,61,62,63,64,65,69,70,71,72,73,74,76,77,78,79,82,83,84,85,86,88,89,90,91,92,93]],
+['Двухсимвольный',[]],
+['страны',[3,4,5,6,7]],
+['согласно',[3,4,5,6,7]],
+['iso-3166',[3,4,5,6,7]],
+['wikipedia',[3,4,5,6,7]],
+['wiki',[3,4,5,6,7]],
+['alpha-2',[3,4,5,6,7]],
+['officially',[3,4,5,6,7]],
+['assigned',[3,4,5,6,7]],
+['elements',[3,4,5,6,7]],
+['simpletype',[3,4,5,6,7,15,16,17,18,43,45,76,77,81,83,84]],
+['restriction',[3,4,5,6,7,15,16,17,18,43,45,76,77,81,83,84]],
+['base',[3,4,5,6,7,15,16,17,18,43,45,76,77,81,83,84]],
+['pattern',[3,4,5,6,7]],
+['value',[3,4,5,6,7,15,16,17,18,33,35,43,45,76,77,81,83,84]],
+['placeofbirth',[3,4,13]],
+['firstname',[3,4,9]],
+['lastname',[3,4,11]],
+['attribute',[3,4,15,16,17,18,43,44,45,76,77,80,81]],
+['required',[3,4,15,16,17,18,42,43,44,45,47,48,76,77,80,81]],
+['enumeration',[3,4,15,16,17,18,43,45,76,77,81,83,84]],
+['nationalpassport',[3,4,15]],
+['internationalpassport',[3,4,15]],
+['--created',[16]],
+['with',[16]],
+['liquid',[16]],
+['studio',[16]],
+['https',[16]],
+['liquid-technologies',[16]],
+['xmlschema-instance',[16]],
+['services',[16]],
+['attributeformdefault',[16]],
+['unqualified',[16]],
+['import',[16]],
+['schemalocation',[16]],
+['namespace',[16]],
+['russian',[16,29,30,32,38]],
+['english',[16,29,30,31,38]],
+['country',[16,27,36,37,97]],
+['city',[16,20,21,24,25,95]],
+['unsignedint',[16,17,19,20,22,25,26,39,40,43,54,55,59,60,70,82,85,86]],
+['airport',[16,17,18,20,23,24,52,53,94,95]],
+['railwaystation',[16,17,18,36,94,97]],
+['airportref',[16,23,43,49,50,51,52,53]],
+['railwaystationref',[16,39,43,56,57,58]],
+['aeroexpressstationref',[16,17,43,46,47,48]],
+['transferref',[16,94]],
+['location',[16,94,96]],
+['property',[16,33]],
+['identifier',[16,33,34]],
+['Идентификатор',[]],
+['дополнительного',[16,33,34,35]],
+['данного',[16,33,34,35]],
+['системе',[16,33,34,35]],
+['клиента',[16,33,34,35]],
+['Значение',[]],
+['service',[16,41,42,43,98]],
+['journeynumber',[16,42,43,82]],
+['Номер',[]],
+['поездки',[16,43,82]],
+['businesstripnumber',[16,42,43,70]],
+['командировки',[16,43,70]],
+['ordernumber',[16,42,43,85]],
+['заказа',[16,43,85]],
+['servicenumber',[16,42,43,86]],
+['Порядковый',[]],
+['номер',[16,16,43,43,70,71,73,74,82,85,86]],
+['услуги',[16,43,44,45,59,60,71,86]],
+['authorization',[16,42,43,62]],
+['unbounded',[16,42,43,49,50,51,62,64,65,66,67,76,77]],
+['completedat',[16,43,62,63,64,65,69]],
+['datetime',[16,43,62,63,64,65,69,71,72,75,87]],
+['Дата',[]],
+['время',[16,43,62,63,64,65,69]],
+['завершения',[16,43,62,63,64,65,69]],
+['авторизации',[16,43,62,63,64,65,69]],
+['stages',[16,43,62,64]],
+['Этапы',[]],
+['stage',[16,43,62,64,65]],
+['Этап',[]],
+['этапа',[16,43,62,64,65,69]],
+['authorizers',[16,43,62,64,65,66]],
+['authorizer',[16,43,62,64,65,66,67]],
+['accountemailaddress',[16,43,62,64,65,66,67,68]],
+['operation',[16,42,43,83]],
+['status',[16,43,83,84]],
+['sale',[16,43,83,84]],
+['Продажа',[]],
+['refund',[16,43,83,84]],
+['Возврат',[]],
+['exchange',[16,43,83,84]],
+['Обмен',[]],
+['startsat',[16,42,43,87]],
+['endsat',[16,42,43,75]],
+['analytics',[16,42,43,59]],
+['istravelpolicycompliant',[16,43,59,61]],
+['boolean',[16,43,44,59,61]],
+['Признак',[]],
+['соответствия',[16,43,59,61]],
+['travelpolicy',[16,43,59,61]],
+['Документ',[]],
+['документа',[16,43,71,73,74]],
+['dateissue',[16,43,71,72]],
+['продажи',[16,43,71,72]],
+['exchangednumber',[16,43,71,73]],
+['предыдущего',[16,43,71,73]],
+['авиабилета',[16,43,71,73]],
+['принятого',[16,43,71,73]],
+['обмен',[16,16,43,43,71,73,83,84]],
+['tariff',[16,42,43,90]],
+['total',[16,43,90,92]],
+['decimal',[16,43,90,92,93]],
+['Сумма',[]],
+['totalnovat',[16,43,90,93]],
+['currency',[16,43,90,91]],
+['валюты',[16,43,90,91]],
+['files',[16,42,43,76]],
+['Файлы',[]],
+['документов',[16,43,76]],
+['file',[16,43,76,77]],
+['Название',[]],
+['файла',[16,43,76,77,79]],
+['link',[16,43,76,77,78]],
+['anyuri',[16,43,76,77,78]],
+['Ссылка',[]],
+['файл',[16,43,76,77,78]],
+['language',[16,43,76,77,81]],
+['contenttype',[16,43,76,77,80]],
+['choice',[16,43]],
+['avia',[16,42,43,45,49]],
+['legs',[16,43,49,50]],
+['departurefrom',[16,43,46,48,49,50,51,53,56,58]],
+['arrivalto',[16,43,46,47,49,50,51,52,56,57]],
+['rail',[16,42,43,45,56]],
+['mixed',[16,43,56]],
+['hotel',[16,42,43,45,54]],
+['cityid',[16,43,54,55]],
+['aeroexpress',[16,42,43,45,46]],
+['stcd1',[16,42,43,88]],
+['unsignedlong',[16,43,88,89]],
+['stcd3',[16,42,43,89]],
+['isoffline',[16,42,43,44]],
+['оффлайн',[16,43,44]],
+['report',[16,41,98]]
+ ];
+ return w;
+}
+        
